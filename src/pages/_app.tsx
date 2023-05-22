6296a0994617dbe3ebd7828b6a5e6c28fceaa40a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-05-22 15:21:43 -0300

    configurando a fonte Roboto no projeto e o stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  style: ["normal"],
+  subsets: ["latin"]
+})
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={roboto.className} >
+      <Component {...pageProps} />
+    </div>
+  )
 }
