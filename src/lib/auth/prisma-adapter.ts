697c310884c5c7d862d373d2a3e642c874857b5e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-05-29 15:25:45 -0300

    criando o método getUserByAccount do PrismaAdapter

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -21,6 +21,7 @@ export function PrismaAdapter(): Adapter {
         avatar_url: user.avatar_url!,
       }
     },
+
     async getUserByEmail(email) {
       const user = await prisma.user.findUniqueOrThrow({
         where: {
@@ -37,16 +38,48 @@ export function PrismaAdapter(): Adapter {
         avatar_url: user.avatar_url!,
       }
     },
-    async getUserByAccount({ providerAccountId, provider }) {},
+
+    async getUserByAccount({ providerAccountId, provider }) {
+      const { user } = await prisma.account.findUniqueOrThrow({
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id: providerAccountId,
+          },
+        },
+        include: {
+          user: true,
+        },
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      }
+    },
+
     async updateUser(user) {},
+
     async deleteUser(userId) {},
+
     async linkAccount(account) {},
+
     async unlinkAccount({ providerAccountId, provider }) {},
+
     async createSession({ sessionToken, userId, expires }) {},
+
     async getSessionAndUser(sessionToken) {},
+
     async updateSession({ sessionToken }) {},
+
     async deleteSession(sessionToken) {},
+
     async createVerificationToken({ identifier, expires, token }) {},
+
     async useVerificationToken({ identifier, token }) {},
   }
 }
