349cbd76d26a94d0343d989122d85cf5e5687f57
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-01 14:42:56 -0300

    fazendo a navegação entre os meses

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,21 +8,40 @@ import {
   CalendarTitle,
 } from './styles'
 import { getWeekDays } from '@/src/utils/get-week-days'
+import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Calendar() {
   const shortWeekDays = getWeekDays({ short: true })
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth() {
+    const previousMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous Month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next Month">
             <CaretRight />
           </button>
         </CalendarActions>
